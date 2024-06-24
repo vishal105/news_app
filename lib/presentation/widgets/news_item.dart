ddddc6cac6d7f31dda83b1926b7c67ed46f50f79
Vishal Sojitra <vishalsojitra.dh@gmail.com>
2024-06-24 09:57:31 +0530

    animation added

--- lib/presentation/widgets/news_item.dart
+++ lib/presentation/widgets/news_item.dart
@@ -1,11 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:news_app/domain/entities/news.dart';
-import 'package:news_app/presentation/pages/news_detail_page.dart';
 
-/// Widget for displaying a news item in a list.
 class NewsItem extends StatelessWidget {
   final News news;
-  static const String placeholderImage = 'assets/images/placeholder.png'; // Placeholder image path
+  static const String placeholderImage = 'assets/images/placeholder.png';
 
   NewsItem({required this.news});
 
@@ -16,11 +14,10 @@ class NewsItem extends StatelessWidget {
       margin: EdgeInsets.all(8.0),
       child: InkWell(
         onTap: () {
-          Navigator.push(
+          Navigator.pushNamed(
             context,
-            MaterialPageRoute(
-              builder: (context) => NewsDetailPage(news: news),
-            ),
+            '/newsDetail',
+            arguments: news,
           );
         },
         child: Padding(
@@ -28,36 +25,39 @@ class NewsItem extends StatelessWidget {
           child: Row(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Container(
-                width: 100,
-                height: 100,
-                child: news.urlToImage != null && news.urlToImage.isNotEmpty
-                    ? ClipRRect(
-                  borderRadius: BorderRadius.circular(8.0),
-                  child: Image.network(
-                    news.urlToImage,
+              Hero(
+                tag: news.urlToImage, // Use a unique tag for each image
+                child: Container(
+                  width: 100,
+                  height: 100,
+                  child: news.urlToImage != null && news.urlToImage.isNotEmpty
+                      ? ClipRRect(
+                    borderRadius: BorderRadius.circular(8.0),
+                    child: Image.network(
+                      news.urlToImage,
+                      fit: BoxFit.cover,
+                      loadingBuilder: (BuildContext context, Widget child, ImageChunkEvent? loadingProgress) {
+                        if (loadingProgress == null) return child;
+                        return Center(
+                          child: CircularProgressIndicator(
+                            value: loadingProgress.expectedTotalBytes != null
+                                ? loadingProgress.cumulativeBytesLoaded / (loadingProgress.expectedTotalBytes ?? 1)
+                                : null,
+                          ),
+                        );
+                      },
+                      errorBuilder: (BuildContext context, Object exception, StackTrace? stackTrace) {
+                        return Image.asset(
+                          placeholderImage,
+                          fit: BoxFit.cover,
+                        );
+                      },
+                    ),
+                  )
+                      : Image.asset(
+                    placeholderImage,
                     fit: BoxFit.cover,
-                    loadingBuilder: (BuildContext context, Widget child, ImageChunkEvent? loadingProgress) {
-                      if (loadingProgress == null) return child;
-                      return Center(
-                        child: CircularProgressIndicator(
-                          value: loadingProgress.expectedTotalBytes != null
-                              ? loadingProgress.cumulativeBytesLoaded / (loadingProgress.expectedTotalBytes ?? 1)
-                              : null,
-                        ),
-                      );
-                    },
-                    errorBuilder: (BuildContext context, Object exception, StackTrace? stackTrace) {
-                      return Image.asset(
-                        placeholderImage,
-                        fit: BoxFit.cover,
-                      );
-                    },
                   ),
-                )
-                    : Image.asset(
-                  placeholderImage,
-                  fit: BoxFit.cover,
                 ),
               ),
               SizedBox(width: 8.0),
